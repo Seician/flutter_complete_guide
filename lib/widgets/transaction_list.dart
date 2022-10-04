bcffa8fa32eab3d525ffea224e3ec81a1bcda484
Seician <seician09@yahoo.com>
2022-10-04 21:37:31 +0300

    Making the List Scrollable

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -9,49 +9,54 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      // we need to make transactions to be added dynamic
-      // we will do that with .map method.
-      children: (transactions.map((tx) {
-        return Card(
-          child: Row(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.symmetric(
-                  vertical: 10, 
-                  horizontal: 15),
-                decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.blue,
-                      width: 2)
-                      ),
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  '\$${tx.amount}',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.blue
-                      ),
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    tx.title,
-                    style: TextStyle(
-                      fontSize: 15, 
-                      fontWeight: FontWeight.bold),
+    return Container(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          // we need to make transactions to be added dynamic
+          // we will do that with .map method.
+          children: (transactions.map((tx) {
+            return Card(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                      vertical: 10, 
+                      horizontal: 15),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.blue,
+                          width: 2)
+                          ),
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      '\$${tx.amount}',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.blue
+                          ),
+                    ),
                   ),
-                  Text(DateFormat.yMMMd().format(tx.date),
-                      style: TextStyle(color: Colors.grey)),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        tx.title,
+                        style: TextStyle(
+                          fontSize: 15, 
+                          fontWeight: FontWeight.bold),
+                      ),
+                      Text(DateFormat.yMMMd().format(tx.date),
+                          style: TextStyle(color: Colors.grey)),
+                    ],
+                  )
                 ],
-              )
-            ],
-          ),
-        );
-      }).toList()),
+              ),
+            );
+          }).toList()),
+        ),
+      ),
     );
   }
 }
