1324a05f8facf8dfa6dbe751415c7504b73b9d15
Seician <seician09@yahoo.com>
2022-10-13 18:28:25 +0300

    Adding A ListTitle Widget

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,60 +11,50 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: transactions.isEmpty ? Column(
-          children: [
-            Text('No transaction added yet!', 
-            style:Theme.of(context).textTheme.headline6 ,),
-            SizedBox(
-              height: 10,
-              ),
-            Container(
-              height: 200,
-              child: Image.asset('assets/images/waiting.png', fit: BoxFit.cover,)),
-          ],
-      ) 
-      : ListView.builder(
-        // we need to make transactions to be added dynamic
-        // we will do that with .map method.
-        itemBuilder: (ctx, index) {
-          return Card(
-            child: Row(
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.symmetric(
-                    vertical: 10,
-                    horizontal: 15),
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Theme.of(context).primaryColorDark, 
-                        width: 2)),
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    '\$${transactions[index].amount.toStringAsFixed(2)}',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Theme.of(context).primaryColorDark),
-                  ),
+      child: transactions.isEmpty
+          ? Column(
+              children: [
+                Text(
+                  'No transaction added yet!',
+                  style: Theme.of(context).textTheme.headline6,
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      transactions[index].title,
-                      style:
-                          Theme.of(context).textTheme.headline6,
-                    ),
-                    Text(DateFormat.yMMMd().format(transactions[index].date),
-                        style: TextStyle(color: Colors.grey)),
-                  ],
-                )
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                    height: 200,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    )),
               ],
+            )
+          : ListView.builder(
+              // we need to make transactions to be added dynamic
+              // we will do that with .map method.
+              itemBuilder: (ctx, index) {
+                return Card(
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 5,
+                    
+                    ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: EdgeInsets.all(6),
+                        child: FittedBox(child: Text('\$${transactions[index].amount}'))),
+                    ),
+                    title: Text(transactions[index].title,
+                    style: Theme.of(context).textTheme.headline6,),
+                    subtitle: Text(DateFormat.yMMMd().format(transactions[index].date),),
+                  ),
+                );
+              },
+              itemCount: transactions.length,
             ),
-          );
-        },
-        itemCount: transactions.length,
-      ),
     );
   }
 }
